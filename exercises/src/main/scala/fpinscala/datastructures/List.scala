bc45bdc13ef1489679e43c2f8519b3fb4090f5ef
gardncl <gardncl@gmail.com>
2017-10-14 18:04:49 -0400

    3.12

--- exercises/src/main/scala/fpinscala/datastructures/List.scala
+++ exercises/src/main/scala/fpinscala/datastructures/List.scala
@@ -12,20 +12,20 @@ case class Cons[+A](head: A, tail: List[A]) extends List[A]
 object List { // `List` companion object. Contains functions for creating and working with lists.
   def sum(ints: List[Int]): Int = ints match { // A function that uses pattern matching to add up a list of integers
     case Nil => 0 // The sum of the empty list is 0.
-    case Cons(x,xs) => x + sum(xs) // The sum of a list starting with `x` is `x` plus the sum of the rest of the list.
+    case Cons(x, xs) => x + sum(xs) // The sum of a list starting with `x` is `x` plus the sum of the rest of the list.
   }
 
   def product(ds: List[Double]): Double = ds match {
     case Nil => 1.0
     case Cons(0.0, _) => 0.0
-    case Cons(x,xs) => x * product(xs)
+    case Cons(x, xs) => x * product(xs)
   }
 
   def apply[A](as: A*): List[A] = // Variadic function syntax
     if (as.isEmpty) Nil
     else Cons(as.head, apply(as.tail: _*))
 
-  val x = List(1,2,3,4,5) match {
+  val x = List(1, 2, 3, 4, 5) match {
     case Cons(x, Cons(2, Cons(4, _))) => x
     case Nil => 42
     case Cons(x, Cons(y, Cons(3, Cons(4, _)))) => x + y
@@ -36,17 +36,17 @@ object List { // `List` companion object. Contains functions for creating and wo
   def append[A](a1: List[A], a2: List[A]): List[A] =
     a1 match {
       case Nil => a2
-      case Cons(h,t) => Cons(h, append(t, a2))
+      case Cons(h, t) => Cons(h, append(t, a2))
     }
 
-  def foldRight[A,B](as: List[A], z: B)(f: (A, B) => B): B = // Utility functions
+  def foldRight[A, B](as: List[A], z: B)(f: (A, B) => B): B = // Utility functions
     as match {
       case Nil => z
       case Cons(x, xs) => f(x, foldRight(xs, z)(f))
     }
 
   def sum2(ns: List[Int]) =
-    foldRight(ns, 0)((x,y) => x + y)
+    foldRight(ns, 0)((x, y) => x + y)
 
   def product2(ns: List[Double]) =
     foldRight(ns, 1.0)(_ * _) // `_ * _` is more concise notation for `(x,y) => x * y`; see sidebar
@@ -71,14 +71,14 @@ object List { // `List` companion object. Contains functions for creating and wo
     l match {
       case Nil => Nil
       case _ if n == 0 => l
-      case Cons(_, t) if n > 0 => drop(t,n-1)
+      case Cons(_, t) if n > 0 => drop(t, n - 1)
     }
   }
 
   @annotation.tailrec
   def dropWhile[A](l: List[A], f: A => Boolean): List[A] = {
     l match {
-      case Cons(h, t) if f(h) => dropWhile(t,f)
+      case Cons(h, t) if f(h) => dropWhile(t, f)
       case _ => l
     }
   }
@@ -91,40 +91,67 @@ object List { // `List` companion object. Contains functions for creating and wo
     }
   }
 
-  def length[A](l: List[A]): Int = foldRight(l, 0)((_,b) => b + 1)
+  def length[A](l: List[A]): Int = foldRight(l, 0)((_, b) => b + 1)
 
   @annotation.tailrec
-  def foldLeft[A,B](l: List[A], z: B)(f: (B, A) => B): B = {
+  def foldLeft[A, B](l: List[A], z: B)(f: (B, A) => B): B = {
     l match {
       case Nil => z
-      case Cons(_, tail) if tail == Nil => z
-      case Cons(h, tail) => foldLeft(tail, f(z,h))(f)
+      case Cons(h, tail) => foldLeft(tail, f(z, h))(f)
     }
   }
 
-  def map[A,B](l: List[A])(f: A => B): List[B] = ???
+  def foldRight2[A, B](as: List[A], z: B)(f: (A, B) => B): B = {
+    foldLeft(reverse(as), z)((b,a) => f(a,b))
+  }
 
+  def map[A, B](l: List[A])(f: A => B): List[B] = ???
+
+  def reverse[A](l: List[A]): List[A] =
+    foldLeft(l, List[A]())((acc, h) => Cons(h, acc))
+
+  def toString[A](l: List[A]): Unit = {
+    l match {
+      case Nil => Nil
+      case Cons(h,t) => {
+        print(h+" ")
+        toString(t)
+      }
+    }
+  }
 }
 
 object Runner {
   def main(args: Array[String]): Unit = {
-    val list = List(1,2,3,4,5)
+    val list = List(1, 2, 3, 4, 5)
 
     //EXERCISE 3.8
-    println(foldRight(List(1,2,3), Nil:List[Int])(Cons(_,_)))
+    println(foldRight(List(1, 2, 3), Nil: List[Int])(Cons(_, _)))
 
     //EXERCISE 3.9
-    println("Length: "+list)
+    println("Length: ")
+    List.toString(list)
     println(List.length(list))
 
     //EXERCISE 3.11
-    println("Sum: "+list)
-    println(List.foldLeft(list, 0)((a,b) => a + b))
-    println("Product: "+list)
-    println(List.foldLeft(list, 1)((a,b) => a * b))
-    println("Length: "+list)
-    println(List.foldLeft(list, 0)((_,b) => b + 1))
+    println("Sum: ")
+    List.toString(list)
+    println(List.foldLeft(list, 0)((a, b) => a + b))
+    println("Product: ")
+    List.toString(list)
+    println(List.foldLeft(list, 1)((a, b) => a * b))
+    println("Length: ")
+    List.toString(list)
+    println(List.foldLeft(list, 0)((_, b) => b + 1))
 
+    //EXERCISE 3.12
+    List.toString(List.reverse(list))
+
+    //EXERCISE 3.13
+    println("\nFold right:")
+    List.toString(List.foldRight(List(1, 2, 3), Nil: List[Int])(Cons(_, _)))
+    println("\nFold right using fold left:")
+    List.toString(List.foldRight2(List(1, 2, 3), Nil: List[Int])(Cons(_, _)))
 
   }
 }
